c437c90bc7ed7bc0ad4e0df243dcaa37e584c131
abdo mohammed <3bdomo7ammed2002@gmail.com>
2025-05-10 16:36:27 +0300

    add index2.js

--- script/index2.js
+++ script/index2.js
@@ -1,3 +1,19 @@
 window.addEventListener("load", function () {
-  console.log("index2");
+  // make the account default to log in if the user is anonymous
+  //select all items
+  let anchorInLis = this.document.querySelectorAll(".main-nav >  ul > li > a");
+  //add event click
+  anchorInLis.forEach(function (a) {
+    if (a.getAttribute("id") != "cart-icon") {
+      a.addEventListener("click", function () {
+        a.nextElementSibling.classList.toggle("active");
+      });
+      document.addEventListener("click", (e) => {
+        if (e.target != a) {
+          a.nextElementSibling.classList.remove("active");
+        }
+      });
+    }
+  });
+  //if click i
 });
